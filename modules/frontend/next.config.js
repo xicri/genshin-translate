cda4dae45c71f73aaf620eaae3e83afe2ee968a4
Xicri <xicri@proton.me>
2022-08-11 13:41:08 +0900

    Revert "chore: temporarily change backend endpoint"
    This reverts commit 69f5b049e17766fa1602eb175cb4ad54cab68fb3.

--- modules/frontend/next.config.js
+++ modules/frontend/next.config.js
@@ -1,9 +1,9 @@
 let backendBaseURL;
 
 if (process.env.NODE_ENV === "production") {
-  backendBaseURL = "https://genshin-translate-production.xicri.workers.dev";
+  backendBaseURL = "https://translate-backend.genshin-dictionary.com";
 } else if (process.env.NODE_ENV === "preview") {
-  backendBaseURL = "https://genshin-translate-preview.xicri.workers.dev";
+  backendBaseURL = "https://translate-backend-preview.genshin-dictionary.com";
 } else if (process.env.NODE_ENV === "development") {
   backendBaseURL = "http://127.0.0.1:8787";
 } else {
